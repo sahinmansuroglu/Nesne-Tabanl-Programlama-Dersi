e97660a71f0041868ee08feeaf542d0bd353c8ee
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-17 14:57:21 +0300

    Create 5.KosulOrnek1.cs

---
+++ 5.KosulOrnek1.cs
@@ -0,0 +1,15 @@
+            int puan1 = 65;
+            int puan2 = 10;
+            double ort = (puan1 + puan2) / 2.0;
+
+            Console.WriteLine($"Ortalama={ort}");
+            
+            if (ort >= 50)
+            {
+                    Console.WriteLine("Geçti");
+            }
+            else
+            {
+                    Console.WriteLine("Kaldı");
+
+            }

c8570c026fd6b7efb6cbed0c5a304b3fcb144871
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-22 11:30:43 +0300

    Create 11.BreakveContinue_1.cs

---
+++ 11.BreakveContinue_1.cs
@@ -0,0 +1,18 @@
+ class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    
+    

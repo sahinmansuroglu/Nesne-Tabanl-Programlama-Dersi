57b1d69d3c959e8bbd8d7ace5d9519490ec91dd0
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-22 10:11:49 +0300

    Create 9.While_DoWhileDongusuOrnek2.cs

---
+++ 9.While_DoWhileDongusuOrnek2.cs
@@ -0,0 +1,16 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i<20)
+            {
+
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
